a84f9b38605f7197d1f6240c53605ada00782b86
lartpang <lartpang@163.com>
2020-08-16 21:36:51 +0800

    * Add a decorator to print the time of an epoch.

--- code/utils/solver.py
+++ code/utils/solver.py
@@ -128,59 +128,7 @@ class Solver:
             train_loss_record = AvgMeter()
 
             for train_batch_id, train_data in enumerate(self.tr_loader):
-                curr_iter = curr_epoch * len(self.tr_loader) + train_batch_id
-
-                self.opti.zero_grad()
-                train_inputs, train_masks, _ = train_data
-                train_inputs = train_inputs.to(self.dev, non_blocking=True)
-                train_masks = train_masks.to(self.dev, non_blocking=True)
-                train_preds = self.net(train_inputs)
-
-                train_loss, loss_item_list = get_total_loss(
-                    train_preds, train_masks, self.loss_funcs
-                )
-                if self.amp:
-                    with self.amp.scale_loss(train_loss, self.opti) as scaled_loss:
-                        scaled_loss.backward()
-                else:
-                    train_loss.backward()
-                self.opti.step()
-
-                if self.arg_dict["sche_usebatch"]:
-                    self.sche.step()
-
-                # 仅在累计的时候使用item()获取数据
-                train_iter_loss = train_loss.item()
-                train_batch_size = train_inputs.size(0)
-                train_loss_record.update(train_iter_loss, train_batch_size)
-
-                # 显示tensorboard
-                if (
-                    self.arg_dict["tb_update"] > 0
-                    and (curr_iter + 1) % self.arg_dict["tb_update"] == 0
-                ):
-                    self.tb_recorder.record_curve("trloss_avg", train_loss_record.avg, curr_iter)
-                    self.tb_recorder.record_curve("trloss_iter", train_iter_loss, curr_iter)
-                    self.tb_recorder.record_curve("lr", self.opti.param_groups, curr_iter)
-                    self.tb_recorder.record_image("trmasks", train_masks, curr_iter)
-                    self.tb_recorder.record_image("trsodout", train_preds.sigmoid(), curr_iter)
-                    self.tb_recorder.record_image("trsodin", train_inputs, curr_iter)
-
-                # 记录每一次迭代的数据
-                if (
-                    self.arg_dict["print_freq"] > 0
-                    and (curr_iter + 1) % self.arg_dict["print_freq"] == 0
-                ):
-                    lr_str = ",".join(
-                        [f"{param_groups['lr']:.7f}" for param_groups in self.opti.param_groups]
-                    )
-                    log = (
-                        f"[I:{curr_iter}/{self.iter_num}][E:{curr_epoch}:{self.end_epoch}]>"
-                        f"[{self.exp_name}][Lr:{lr_str}][Avg:{train_loss_record.avg:.5f}|Cur:{train_iter_loss:.5f}|"
-                        f"{loss_item_list}]"
-                    )
-                    print(log)
-                    write_data_to_file(log, self.path_dict["tr_log"])
+                self._train_per_epoch(curr_epoch, train_batch_id, train_data, train_loss_record)
 
             # 根据周期修改学习率
             if not self.arg_dict["sche_usebatch"]:
@@ -206,6 +154,54 @@ class Solver:
         else:
             self.test()
 
+    @Timer
+    def _train_per_epoch(self, curr_epoch, train_batch_id, train_data, train_loss_record):
+        curr_iter = curr_epoch * len(self.tr_loader) + train_batch_id
+
+        self.opti.zero_grad()
+
+        train_inputs, train_masks, _ = train_data
+        train_inputs = train_inputs.to(self.dev, non_blocking=True)
+        train_masks = train_masks.to(self.dev, non_blocking=True)
+        train_preds = self.net(train_inputs)
+
+        train_loss, loss_item_list = get_total_loss(train_preds, train_masks, self.loss_funcs)
+        if self.amp:
+            with self.amp.scale_loss(train_loss, self.opti) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            train_loss.backward()
+        self.opti.step()
+
+        if self.arg_dict["sche_usebatch"]:
+            self.sche.step()
+
+        # 仅在累计的时候使用item()获取数据
+        train_iter_loss = train_loss.item()
+        train_batch_size = train_inputs.size(0)
+        train_loss_record.update(train_iter_loss, train_batch_size)
+
+        # 显示tensorboard
+        if self.arg_dict["tb_update"] > 0 and (curr_iter + 1) % self.arg_dict["tb_update"] == 0:
+            self.tb_recorder.record_curve("trloss_avg", train_loss_record.avg, curr_iter)
+            self.tb_recorder.record_curve("trloss_iter", train_iter_loss, curr_iter)
+            self.tb_recorder.record_curve("lr", self.opti.param_groups, curr_iter)
+            self.tb_recorder.record_image("trmasks", train_masks, curr_iter)
+            self.tb_recorder.record_image("trsodout", train_preds.sigmoid(), curr_iter)
+            self.tb_recorder.record_image("trsodin", train_inputs, curr_iter)
+        # 记录每一次迭代的数据
+        if self.arg_dict["print_freq"] > 0 and (curr_iter + 1) % self.arg_dict["print_freq"] == 0:
+            lr_str = ",".join(
+                [f"{param_groups['lr']:.7f}" for param_groups in self.opti.param_groups]
+            )
+            log = (
+                f"[I:{curr_iter}/{self.iter_num}][E:{curr_epoch}:{self.end_epoch}]>"
+                f"[{self.exp_name}][Lr:{lr_str}][Avg:{train_loss_record.avg:.5f}|Cur:{train_iter_loss:.5f}|"
+                f"{loss_item_list}]"
+            )
+            print(log)
+            write_data_to_file(log, self.path_dict["tr_log"])
+
     def test(self):
         self.net.eval()
 
@@ -222,7 +218,7 @@ class Solver:
             if not os.path.exists(self.save_path):
                 construct_print(f"{self.save_path} do not exist. Let's create it.")
                 os.makedirs(self.save_path)
-            results = self.__test_process(save_pre=self.save_pre)
+            results = self._test_process(save_pre=self.save_pre)
             msg = f"Results on the testset({data_name}:'{data_path}'): {results}"
             construct_print(msg)
             write_data_to_file(msg, self.path_dict["te_log"])
@@ -235,7 +231,7 @@ class Solver:
             # save result into xlsx file.
             self.xlsx_recorder.write_xlsx(self.exp_name, total_results)
 
-    def __test_process(self, save_pre):
+    def _test_process(self, save_pre):
         loader = self.te_loader
 
         pres = [AvgMeter() for _ in range(256)]
